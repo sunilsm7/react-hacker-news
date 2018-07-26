fbc6343279cc8148e76daf406c5fce08dd8e4136
Sunil <sunil9766@gmail.com>
2018-07-26 18:21:51 +0530

    prev state management

--- src/App.js
+++ src/App.js
@@ -265,6 +265,29 @@ Table.propTypes = {
   showMessage: PropTypes.func,
 }
 
+
+const updateSearchTopStoriesState = (hits, page) => (prevState) => {
+  const { searchKey, results } = prevState;
+
+  const oldHits = results && results[searchKey]
+    ? results[searchKey].hits
+    : [];
+
+  const updatedHits = [
+    ...oldHits,
+    ...hits
+  ];
+
+  this.setState({ 
+    results: { 
+      ...results,
+      [searchKey]: { hits: updatedHits, page }
+    } ,
+    isLoading: false
+  });
+};
+
+
 class App extends Component {
   _isMounted = false;
 
@@ -294,25 +317,7 @@ class App extends Component {
 
   setSearchTopStories(result){
     const { hits, page } = result;
-    const { searchKey, results } = this.state;
-
-    const oldHits = results && results[searchKey]
-      ? results[searchKey].hits
-      : [];
-
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ];
-
-    this.setState({ 
-      results: { 
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      } ,
-      isLoading: false
-    });
-
+    this.setState(updateSearchTopStoriesState(hits, page));
   }
 
   fetchSearchTopStories(searchTerm, page=0) {
